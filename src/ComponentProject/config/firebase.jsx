0cfaf1d3aa1ac3292dedf7576838446e76196dd2
karanwaghel <153583494+karanwaghel@users.noreply.github.com>
2026-02-27 12:26:19 +0530

    Create firebase.jsx

---
+++ src/ComponentProject/config/firebase.jsx
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import {getAuth,GoogleAuthProvider} from 'firebase/auth'
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDuK1bljcwzRMLqzg7I0UbSnu9OuDfvtUM",
+  authDomain: "trial-base-820ac.firebaseapp.com",
+  projectId: "trial-base-820ac",
+  storageBucket: "trial-base-820ac.firebasestorage.app",
+  messagingSenderId: "185047279616",
+  appId: "1:185047279616:web:decaecea4f07d2134550e5",
+  measurementId: "G-SFNBZKFVTJ"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const GoogleProvider =new GoogleAuthProvider();
